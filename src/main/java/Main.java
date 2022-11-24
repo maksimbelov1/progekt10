03381613aa65ae25cd2ba6427b783bdd7661eee1
belovmv.24 <maksimbelov2006@gmail.com>
2022-11-24 13:00:07 +0300

    classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,10 @@
-package org.example;
+import app.Application;
+import io.github.humbleui.jwm.App;
 
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        App.start(Application::new);
     }
+
 }
